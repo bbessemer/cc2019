a9d21e84b55eedb12c516c877ef72d8c7d0b8879
Seth <rsethc@gmail.com>
2019-01-26 11:05:31 -0500

    fixed derpy tabs

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// SERVER SIDE 
+// SERVER SIDE
 
 const {app, BrowserWindow} = require('electron')
 
@@ -26,17 +26,17 @@ function createWindow()
 app.on('ready', createWindow)
 
 app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+	// On macOS it is common for applications and their menu bar
+	// to stay active until the user quits explicitly with Cmd + Q
+	if (process.platform !== 'darwin') {
+		app.quit()
+	}
 })
 
 app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (window === null) {
-    createWindow()
-  }
+	// On macOS it's common to re-create a window in the app when the
+	// dock icon is clicked and there are no other windows open.
+	if (window === null) {
+		createWindow()
+	}
 })
