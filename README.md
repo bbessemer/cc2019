7fc8d3343bbc8d9c57b527655de027d43be54e4d
Surafel Assefa <surafel911@gmail.com>
2019-01-25 23:18:16 -0500

    Inital setup code.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # cc2019
+
+## Setup
+
+Install electron by runnning: `sudo npm -g install electron`. If there is an
+issue with permissions, add the `--unsafe-perm` flag to the command.
---
+++ index.js
@@ -0,0 +1,34 @@
+const {app, BrowserWindow} = require('electron')
+
+let window
+
+function createWindow()
+{
+	window = new BrowserWindow({width: 1280, height: 720})
+
+	window.loadFile('index.html')
+
+	window.on('closed', () => {
+		// Derefencing window object will 
+		window = null
+	})
+}
+
+// This method will be called when Electron finished initializing
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (window === null) {
+    createWindow()
+  }
+})
