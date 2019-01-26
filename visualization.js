59ddbca42ac60fc03b7e2447cad31cd2009abac7
Seth <rsethc@gmail.com>
2019-01-26 13:20:53 -0500

    visualization changes & index.html has link to that testing file

--- visualization.js
+++ visualization.js
@@ -1,6 +1,8 @@
 /*
 	visualizer (client-side) script
 
+	API, basically.
+
 	functions for adding stack frame into the view, adding things to a stack frame, and removing existing stack frame
 
 */
@@ -9,13 +11,26 @@ function NewInt ()
 {
 }
 
-
+var StackFramesZone = document.getElementById("StackFramesZone");
+var TopLevelStackFrame = null;
 function NewStackFrame ()
 {
 	// insert a new element into the page *before* the previous stack frame, and give it an animation that makes its
 	// transform's height go from 0% to 100% to visually smoothly push down the previous stack frame.
 
-
+	var stackframe = document.createElement("div");
+	stackframe.className = "StackFrame";
+	if (TopLevelStackFrame === null)
+	{
+		StackFramesZone.appendChild(stackframe);
+		TopLevelStackFrame = stackframe;
+	}
+	else
+	{
+		TopLevelStackFrame.insertBefore(stackframe);
+	}
+  	element.classList.add("Expanded");
+	return stackframe;
 }
 
 function PopStack ()
